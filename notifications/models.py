f7b6f3206acd7b9a07142ec55d7d1e1b191c03ea
PhinehasdFalken <uchegbuphinehas2980@gmail.com>
2025-12-25 16:12:54 +0100

    Delivery log instantiated at every task execution

--- notifications/models.py
+++ notifications/models.py
@@ -3,9 +3,20 @@ from users.models import NewUser
 
 
 class DeliveryLog(models.Model):
+  STATUS_CHOICES = (
+    ("pending", "Pending"),
+    ("success", "Success"),
+    ("failed", "Failed"),
+  )
+
   user = models.ForeignKey(NewUser, on_delete=models.CASCADE)
   channel = models.CharField(max_length=20)
   event_type = models.CharField(max_length=50)
-  status = models.CharField(max_length=20)
+  status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+  error_message = models.TextField(blank=True, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
   message = models.TextField(null=True, blank=True)
+
+
+  def __str__(self):
+    return f"{self.user.email} | {self.event_type} | {self.channel} | {self.status}"
--- notifications/tasks.py
+++ notifications/tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 from .services.factory import NotificationFactory
+from users.models import NewUser
+from notifications.models import DeliveryLog
 
 # the shared_task decorator turns the function below it into a celery task.
 # Celery can now run this function in the backgroung insated of Django running it.
@@ -19,8 +21,25 @@ def send_activation_email_task(self, recipient, subject, message):
 
 @shared_task(bind=True, max_retries=3)
 def send_notification_task(self, user_id, event_type, channel):
+  user = NewUser.objects.get(pk=user_id)
+
+  log = DeliveryLog.objects.create(
+    user=user,
+    event_type=event_type,
+    channel=channel,
+    status="pending",
+  )
+
   try:
     handler = NotificationFactory.get_handler(channel)
     handler.send(user_id, event_type)
+
+    log.status = "success"
+    log.save(update_fields=["status"])
+
   except Exception as exc:
+    log.status = "failed"
+    log.error_message = str(exc)
+    log.save(update_fields=["status", "error_message"])
+    
     raise self.retry(exc=exc, countdowm=30)
